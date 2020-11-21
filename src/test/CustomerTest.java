45d088c4751c08ebd0ffef11c10e40eb9ae389a8
scarlens250 <74828654+scarlens250@users.noreply.github.com>
2020-11-21 19:48:17 +0200

    Add files via upload

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,54 +1,15 @@
-package domain;
-
-public class Customer {
-    private int ID;
-    private boolean isNew;
-    private double total;
+package test;
+import domain.Customer;
+public class CustomerTest {
     
-    public Customer(){
-            ID=21;
-            isNew=true;
-            total=450;
-            
-        }
-    
-    public void displayCustomerInfo(){
-        System.out.println("ID= "+ID);
-        System.out.println("isNew= "+isNew);
-        System.out.println("Total purchases are= "+ total);
-    }
-    
-    public int getID() {
-        return ID;
-    }
-
-    public void setID(int newID) {
-      if( newID >= 0){
-        ID = newID;}
-      else{
-        System.out.println("Error: newID:"+newID +" < 0 ");
-      }
-    }
-
-    public boolean getIsNew() {
-        return isNew;
-    }
-
-    public void setIsNew(boolean newIsNew) {
-        isNew = newIsNew;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double newTotal) {
-      if( newTotal >= 0){
-        total = newTotal;
-      }
-      else{
-        System.out.println("Error: newTotal:"+newTotal +" < 0 ");
-      }
+    public static void main(String[] args){
+        Customer cus = new Customer();
+        cus.displayCustomerInfo();
+        cus.setIsNew(false);
+        cus.setID(21);
+        cus.setTotal(993.12);
+        cus.displayCustomerInfo();
+        
     }
     
 }
